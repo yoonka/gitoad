de1db9930a1e8e69a875d97fb3a1810e7c16413d
Grzegorz Junka <list1@gjunka.com>
2015-08-02 20:30:51 +0000

    First stub version

---
+++ src/gioa_api.erl
@@ -0,0 +1,63 @@
+%% Copyright (c) 2015, Grzegorz Junka
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%%
+%% * Redistributions of source code must retain the above copyright notice,
+%%   this list of conditions and the following disclaimer.
+%% * Redistributions in binary form must reproduce the above copyright notice,
+%%   this list of conditions and the following disclaimer in the documentation
+%%   and/or other materials provided with the distribution.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+%% EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-module(gioa_api).
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-include_lib("yolf/include/yolf.hrl").
+
+-define(SERVER, ?MODULE).
+
+%%% API
+start_link() ->
+    ?LOG_WORKER(?SERVER),
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%% gen_server callbacks
+init([]) ->
+    ?LOG_WORKER_INIT(?SERVER),
+    {ok, []}.
+
+handle_call(_, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_, State) ->
+    {noreply, State}.
+
+handle_info(_, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%% Internal methods
